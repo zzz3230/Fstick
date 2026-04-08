5b4263e7edba97bcb388428319e7644b3842327d
FoxyTimes <qawwaq2@mail.ru>
2026-04-09 00:29:30 +0700

    develop started

---
+++ Fstick registry service/src/main/java/ru/fstick/registry_service/dto/PluginData.java
@@ -0,0 +1,24 @@
+package ru.fstick.registry_service.dto;
+
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Builder
+public class PluginData {
+    private UUID id;
+    private UUID creatorId;
+    private String name;
+    private String description;
+    private String category;
+    private List<String> tags;
+    private String currentVersion;
+    private String status;
+    private String iconUrl;
+    private String createdAt;
+    private String updatedAt;
+}
