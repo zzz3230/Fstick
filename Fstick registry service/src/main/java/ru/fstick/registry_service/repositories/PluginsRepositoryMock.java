835e591f03308d1acb86066ca56843f1337ded8b
FoxyTimes <qawwaq2@mail.ru>
2026-04-09 14:51:15 +0700

    develop continue

---
+++ Fstick registry service/src/main/java/ru/fstick/registry_service/repositories/PluginsRepositoryMock.java
@@ -0,0 +1,83 @@
+package ru.fstick.registry_service.repositories;
+
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import ru.fstick.registry_service.dto.bd.PluginData;
+import ru.fstick.registry_service.dto.bd.Screenshot;
+import ru.fstick.registry_service.dto.bd.Version;
+
+import java.sql.Time;
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Repository
+public class PluginsRepositoryMock {
+    private final JdbcTemplate jdbcTemplate;
+
+    public PluginsRepositoryMock(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    //сделать запрос в базу данных на поиск плагинов
+    public List<PluginData> getPlugins(Integer offset, Integer limit, String category, String search, String sort, String order) {
+
+        PluginData pluginData = PluginData.builder()
+                .id(UUID.randomUUID())
+                .creatorId(UUID.randomUUID())
+                .name("test")
+                .description("mock")
+                .category("social")
+                .tags(new ArrayList<>(Arrays.asList("tag1", "tag2", "tag3")))
+                .currentVersion("1.0.0")
+                .status("active").iconUrlKey("iconurls3key")
+                .createdAt(LocalDateTime.now().toString())
+                .updatedAt(LocalDateTime.now().toString())
+                .build();
+
+        return new ArrayList<>(Collections.singletonList(pluginData));
+        //TODO
+    }
+
+    //сделать запрос в базу данных на количество плагинов
+    public Integer countPlugins(String category, String search) {
+        return 20;
+        //TODO
+    }
+
+    public PluginData addPlugin(@NotBlank String name, @NotBlank String description, String category, List<String> tags, @NotBlank String version, @NotBlank String changelog) {
+        return PluginData.builder()
+                .id(UUID.randomUUID())
+                .creatorId(UUID.randomUUID())
+                .name("test")
+                .description("mock")
+                .category("social")
+                .tags(new ArrayList<>(Arrays.asList("tag1", "tag2", "tag3")))
+                .currentVersion("1.0.0")
+                .status("active").iconUrlKey("iconurls3key")
+                .createdAt(LocalDateTime.now().toString())
+                .updatedAt(LocalDateTime.now().toString())
+                .build();
+        //TODO
+    }
+
+
+    //Получить версии плагина
+    public List<Version> getVersionOfPlugin(UUID id) {
+        return new ArrayList<>(Arrays.asList(Version.builder()
+                .id(UUID.randomUUID())
+                .version("1.0.0")
+                .changelog("created")
+                .createdAt(LocalDateTime.now().toString()).build()));
+        //TODO
+    }
+
+    //Получить скриншоты плагина
+    public List<Screenshot> getScreenshots(UUID id) {
+        return new ArrayList<>(Arrays.asList(Screenshot.builder()
+                .screenshotId(UUID.randomUUID())
+                .s3ScreenshotKey("s3key")
+                .build()));
+        //TODO
+    }
+}
--- Fstick registry service/src/main/java/ru/fstick/registry_service/services/PluginsService.java
+++ Fstick registry service/src/main/java/ru/fstick/registry_service/services/PluginsService.java
@@ -1,27 +1,32 @@
 package ru.fstick.registry_service.services;
 
 import org.springframework.stereotype.Service;
-import ru.fstick.registry_service.dto.api.PaginationData;
+import org.springframework.web.multipart.MultipartFile;
+import ru.fstick.registry_service.dto.api.requests.PluginRequest;
+import ru.fstick.registry_service.dto.api.responsies.PluginResponse;
+import ru.fstick.registry_service.dto.bd.Screenshot;
+import ru.fstick.registry_service.dto.bd.Version;
+import ru.fstick.registry_service.dto.service.PaginationData;
 import ru.fstick.registry_service.dto.bd.PluginData;
-import ru.fstick.registry_service.dto.api.PluginsResponse;
-import ru.fstick.registry_service.repositories.PluginsRepository;
+import ru.fstick.registry_service.dto.api.responsies.PluginsResponse;
+import ru.fstick.registry_service.repositories.PluginsRepositoryMock;
 
 import java.util.List;
 
 @Service
 public class PluginsService {
 
-    private final PluginsRepository pluginsRepository;
+    private final PluginsRepositoryMock pluginsRepositoryMock;
 
-    public PluginsService(PluginsRepository pluginsRepository) {
-        this.pluginsRepository = pluginsRepository;
+    public PluginsService(PluginsRepositoryMock pluginsRepositoryMock) {
+        this.pluginsRepositoryMock = pluginsRepositoryMock;
     }
 
     //получить список плагинов и их пагинацию по критериям
     public PluginsResponse getPlugins(Integer page, Integer limit, String category, String search, String sort, String order) {
         Integer offset = page * limit;
-        List<PluginData> pluginsData = pluginsRepository.getPlugins(offset, limit, category, search, sort, order);
-        Integer pluginsTotal = pluginsRepository.countPlugins(category, search);
+        List<PluginData> pluginsData = pluginsRepositoryMock.getPlugins(offset, limit, category, search, sort, order);
+        Integer pluginsTotal = pluginsRepositoryMock.countPlugins(category, search);
 
         int totalPages = (int) Math.ceil((double) pluginsTotal / limit);
 
@@ -39,4 +44,38 @@ public class PluginsService {
                 .pagination(paginationData)
                 .build();
     }
+
+
+    //добавить плагин
+    public PluginResponse addPlugin(PluginRequest pluginRequest, MultipartFile archive, MultipartFile icon, List<MultipartFile> screenshots) {
+
+        PluginData pluginData = pluginsRepositoryMock.addPlugin(
+                pluginRequest.getName(),
+                pluginRequest.getDescription(),
+                pluginRequest.getCategory(),
+                pluginRequest.getTags(),
+                pluginRequest.getVersion(),
+                pluginRequest.getChangelog());
+
+        List<Version> versions = pluginsRepositoryMock.getVersionOfPlugin(pluginData.getId());
+        List<Screenshot> screenshotsData = pluginsRepositoryMock.getScreenshots(pluginData.getId());
+
+
+        return PluginResponse.builder()
+                .id(pluginData.getId())
+                .creatorId(pluginData.getCreatorId())
+                .name(pluginData.getName())
+                .description(pluginData.getDescription())
+                .category(pluginData.getCategory())
+                .tags(pluginRequest.getTags())
+                .currentVersion(pluginData.getCurrentVersion())
+                .status(pluginData.getStatus())
+                .iconUrl(pluginData.getIconUrlKey())
+                .createdAt(pluginData.getCreatedAt())
+                .updatedAt(pluginData.getUpdatedAt())
+                .versions(versions)
+                .screenshots(screenshotsData)
+                .build();
+    }
+
 }
