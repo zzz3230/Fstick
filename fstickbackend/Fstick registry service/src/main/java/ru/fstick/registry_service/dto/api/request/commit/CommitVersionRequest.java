a6a9e2ff7315007531bb2e55d05d3a2c997a3620
FoxyTimes <qawwaq2@mail.ru>
2026-04-11 23:16:27 +0700

    fixes

--- fstickbackend/Fstick registry service/src/main/java/ru/fstick/registry_service/dto/api/request/commit/CommitVersionRequest.java
+++ fstickbackend/Fstick registry service/src/main/java/ru/fstick/registry_service/dto/api/request/commit/CommitVersionRequest.java
@@ -3,8 +3,11 @@ package ru.fstick.registry_service.dto.api.request.commit;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class CommitVersionRequest {
     private String version;
     private String changelog;
+    private List<String> keys;
 }
